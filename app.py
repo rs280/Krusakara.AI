68a356bec5f23032eed2b4a81462baf600afa7af
rs280 <rishabhsahoo1@gmail.com>
2022-06-17 15:04:22 -0700

    change back

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import neuralprophet as NeuralProphet
-import plotly.graph_objects as go
+
 
 st.set_page_config(
    page_title="କୃଷକ.AI  ",
@@ -14,14 +13,3 @@ st.subheader('First, go to the "Crop Prediction" page to see the prediction of t
 st.subheader('Second, go to the "Future Predictor" page to see the prediction of the future price of the crop you got.')
 
 
-
-data_location = "https://raw.githubusercontent.com/ourownstory/neuralprophet-data/main/datasets/"
-
-df = pd.read_csv(data_location + 'wp_log_peyton_manning.csv')
-
-m = NeuralProphet()
-metrics = m.fit(df)
-future = m.make_future_dataframe(df=df, periods=365)
-forecast = m.predict(df=future)
-fig_forecast = m.plot(forecast)
-st.plotly_chart(fig_forecast)
